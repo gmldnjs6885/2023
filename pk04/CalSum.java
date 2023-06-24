83bd536d0cdcdc64bbfc6756a36e5f9d25b47dad
gmldnjs6885 <128600278+gmldnjs6885@users.noreply.github.com>
2023-06-24 22:01:01 +0900

    Add files via upload

---
+++ pk04/CalSum.java
@@ -0,0 +1,18 @@
+package pk04;
+
+public class CalSum {
+
+	public static void main(String[] args) {
+
+		int i=1;
+		int sum=0;
+		
+		while(i<=10)
+		{
+			sum=sum+i;
+			i++;
+		}
+		System.out.println("гу╟Х "+sum);
+	}
+
+}
