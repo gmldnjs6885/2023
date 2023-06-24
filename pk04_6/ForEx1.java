539a17f3f61342c6d329e022a553b18f76c4416a
gmldnjs6885 <128600278+gmldnjs6885@users.noreply.github.com>
2023-06-24 22:10:36 +0900

    Add files via upload

---
+++ pk04_6/ForEx1.java
@@ -0,0 +1,16 @@
+package pk04_6;
+
+public class ForEx1 {
+//1부터 100까지의 데이터 출력
+	public static void main(String[] args) {
+
+		int i;
+
+		for(i=1; i<=100; i++)
+
+			System.out.printf("%4d",i);
+
+
+	}
+
+}
