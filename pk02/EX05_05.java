024059eb491e320b1c09bf28b6a20ad0230d70a4
gmldnjs6885 <128600278+gmldnjs6885@users.noreply.github.com>
2023-06-24 21:57:12 +0900

    Add files via upload

---
+++ pk02/EX05_05.java
@@ -0,0 +1,23 @@
+package pk02;
+
+public class EX05_05 {
+
+	public static void main(String[] args) {
+
+		int a=200;
+		
+		if(a<100)
+		{
+			System.out.printf("100보다 작군요..\n");
+			System.out.printf("참이면 이 문자옫 보이겠죠?\n");
+		}
+		else 
+		{
+			System.out.printf("100보다 크군요 ..\n");
+			System.out.printf("거짓이면 이 문장도 보이겠죠?\n");
+		}
+		
+		System.out.printf("프로 그램 끝");
+	}
+
+}
---
+++ pk02/Ex05_03.java
@@ -0,0 +1,17 @@
+package pk02;
+
+public class Ex05_03 {
+
+	public static void main(String[] args) {
+		int a=200;
+		
+		if(a<100)
+		{
+			System.out.printf("100보다 작군요.. \n");
+			System.out.printf("거짓이므로 이 분장은 안보이겠죠?\n");
+		}
+		
+		System.out.printf("프로 그램 끝! \n");
+	}
+
+}
