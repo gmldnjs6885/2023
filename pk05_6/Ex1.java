130dbf564b0bdad1a6dbcf56efd916fc3fe7b28f
gmldnjs6885 <128600278+gmldnjs6885@users.noreply.github.com>
2023-06-24 22:13:29 +0900

    Add files via upload

---
+++ pk05_6/Ex1.java
@@ -0,0 +1,30 @@
+package pk05_6;
+import java.util.Scanner;
+public class Ex1 {
+
+	public static void main(String[] args) {
+		Scanner sc;
+		int d;
+		String msg;
+
+		sc = new Scanner(System.in);
+		System.out.println("정수형을 입력하세요");
+		d=sc.nextInt();
+
+		sc.close();
+
+		if(d%2==0)
+			msg="짝수";
+
+		else
+			msg="홀수";
+
+		System.out.printf("입력한 수는 [%d]이고,[%s]입니다 ",d,msg);
+
+
+
+
+
+	}
+
+}
---
+++ pk05_6/Ex5.java
@@ -0,0 +1,26 @@
+package pk05_6;
+
+public class Ex5 {
+	public static void main(String[] args) {
+
+		char c;
+
+		
+
+		for(int i=1; i<=26;i++)
+
+		{
+			c='A';
+			
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(c++ +" ");
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+
+}
