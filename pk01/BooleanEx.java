4eedff387589998868bacc659274d60c96163a73
gmldnjs6885 <128600278+gmldnjs6885@users.noreply.github.com>
2023-06-24 21:56:11 +0900

    Add files via upload
    2023년 한림대학교 1학년 1학기 자바 1 1주차 프로그래밍

---
+++ pk01/BooleanEx.java
@@ -0,0 +1,11 @@
+package pk01;
+
+public class BooleanEx {
+
+	public static void main(String[] args) {
+		boolean a=true, b=false;
+		System.out.println("a의 값은 :" +a);
+		System.out.println("b의 값은 :" +b);
+	}
+
+}
