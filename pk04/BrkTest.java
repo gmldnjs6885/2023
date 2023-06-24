f94a431d2386049264755fdd5fac2db7a914d19c
gmldnjs6885 <128600278+gmldnjs6885@users.noreply.github.com>
2023-06-24 22:08:50 +0900

    Add files via upload

---
+++ pk04/BrkTest.java
@@ -0,0 +1,14 @@
+package pk04;
+
+public class BrkTest {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=10; ++i)
+		{
+			if(i%3==0)
+				break;
+			System.out.print(i+" ");
+		}
+	}
+
+}
